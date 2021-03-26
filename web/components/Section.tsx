0fba7f24d159d80c6cb363d273cf81d9f42673d5
Max Stuart <maxastuart@gmail.com>
2021-03-25 17:40:05 -0700

    use <img> for background. add media query

--- web/components/Section.tsx
+++ web/components/Section.tsx
@@ -1,9 +1,9 @@
-import React, { useEffect, useRef, useState } from 'react';
+import React from 'react';
+import { useTheme } from '@emotion/react';
 import styled from '@emotion/styled';
 import { PortableText, urlFor } from '../config/sanity';
 import { BlockHeading } from './BlockHeading';
 import { Cta } from './Cta';
-import { SectionElement } from './SectionElement';
 import { serializers } from './serializers';
 
 type SectionProps = {
@@ -21,11 +21,32 @@ type SectionProps = {
   text: unknown[];
 };
 
+const SectionWrapper = styled.div`
+  position: relative;
+  display: flex;
+  width: 100%;
+`;
+
+const SectionContents = styled.div`
+  position: absolute;
+  top: 0;
+  left: 0;
+  display: flex;
+  height: 100%;
+  flex-direction: column;
+  justify-content: space-between;
+  object-fit: contain;
+`;
+
 const CenterRow = styled.div`
   display: flex;
   flex: 1 1;
   justify-content: center;
   align-items: center;
+`;
+
+const BackgroundImage = styled.img`
+  width: 100%;
 `;
 
 export const Section: React.FC<SectionProps> = ({
@@ -38,45 +59,49 @@ export const Section: React.FC<SectionProps> = ({
   image,
   text,
 }) => {
-  const width = 100 / [headingLeft, image, headingRight].filter((el) => !!el).length;
+  const width = `${100 / [headingLeft, image, headingRight].filter((el) => !!el).length}%`;
 
+  const { isMobile } = useTheme();
   return (
-    <SectionElement backgroundImage={backgroundImage}>
-      {headingAbove && <BlockHeading blocks={headingAbove} />}
-      <CenterRow>
-        {headingLeft && <BlockHeading blocks={headingLeft} style={{ width }} />}
-        {image && (
-          <div style={{ width }}>
-            <figure>
-              <img
-                alt={image.alt}
-                src={urlFor(image)}
-                style={{
-                  display: 'block',
-                  width: '100%',
-                }}
-              />
-              {image.caption && (
-                <figcaption>
-                  <div>{image.caption}</div>
-                </figcaption>
-              )}
-            </figure>
+    <SectionWrapper>
+      <BackgroundImage alt='' src={urlFor(backgroundImage)} />
+      <SectionContents>
+        {headingAbove && <BlockHeading blocks={headingAbove} />}
+        <CenterRow>
+          {headingLeft && <BlockHeading blocks={headingLeft} style={{ width }} />}
+          {image && (
+            <div style={{ width }}>
+              <figure>
+                <img
+                  alt={image.alt}
+                  src={urlFor(image)}
+                  style={{
+                    display: 'block',
+                    width: '100%',
+                  }}
+                />
+                {image.caption && (
+                  <figcaption>
+                    <div>{image.caption}</div>
+                  </figcaption>
+                )}
+              </figure>
+            </div>
+          )}
+          {headingRight && (
+            <BlockHeading blocks={headingRight} style={{ textAlign: 'right', width }} />
+          )}
+        </CenterRow>
+        {headingBelow && <BlockHeading blocks={headingBelow} />}
+        {text && <PortableText blocks={text} serializers={serializers} />}
+        {ctaButtons && (
+          <div>
+            {ctaButtons.map((cta) => (
+              <Cta {...cta} key={cta._key} />
+            ))}
           </div>
         )}
-        {headingRight && (
-          <BlockHeading blocks={headingRight} style={{ textAlign: 'right', width }} />
-        )}
-      </CenterRow>
-      {headingBelow && <BlockHeading blocks={headingBelow} />}
-      {text && <PortableText blocks={text} serializers={serializers} />}
-      {ctaButtons && (
-        <div>
-          {ctaButtons.map((cta) => (
-            <Cta {...cta} key={cta._key} />
-          ))}
-        </div>
-      )}
-    </SectionElement>
+      </SectionContents>
+    </SectionWrapper>
   );
 };
--- web/pages/_app.tsx
+++ web/pages/_app.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import createCache from '@emotion/cache';
 import { CacheProvider } from '@emotion/react';
-import { CssBaseline, StylesProvider, ThemeProvider } from '@material-ui/core';
+import { CssBaseline, StylesProvider, ThemeProvider, useMediaQuery } from '@material-ui/core';
 import { AppProps } from 'next/app';
 import Head from 'next/head';
 import { appTheme } from '../theme';
@@ -26,20 +26,24 @@ import siteGlobalConfig from './siteGlobalConfig.preval';
 
 export const cache = createCache({ key: 'css' });
 
-const App: React.FC<AppProps> = ({ Component, pageProps }) => (
-  <CacheProvider value={cache}>
-    <Head>
-      <title>Souper Bowl of Caring - TackleHunger.org</title>
-      {/* see: https://github.com/vercel/next.js/blob/master/errors/no-document-viewport-meta.md */}
-      <meta content='minimum-scale=1, initial-scale=1, width=device-width' name='viewport' />
-    </Head>
-    <StylesProvider injectFirst>
-      <ThemeProvider theme={appTheme}>
-        <CssBaseline />
-        <Component {...{ ...pageProps, ...siteGlobalConfig }} />
-      </ThemeProvider>
-    </StylesProvider>
-  </CacheProvider>
-);
+const App: React.FC<AppProps> = ({ Component, pageProps }) => {
+  appTheme.isMobile = useMediaQuery(appTheme.breakpoints.down('sm')); // 600px
+
+  return (
+    <CacheProvider value={cache}>
+      <Head>
+        <title>Souper Bowl of Caring - TackleHunger.org</title>
+        {/* see: https://github.com/vercel/next.js/blob/master/errors/no-document-viewport-meta.md */}
+        <meta content='minimum-scale=1, initial-scale=1, width=device-width' name='viewport' />
+      </Head>
+      <StylesProvider injectFirst>
+        <ThemeProvider theme={appTheme}>
+          <CssBaseline />
+          <Component {...{ ...pageProps, ...siteGlobalConfig }} />
+        </ThemeProvider>
+      </StylesProvider>
+    </CacheProvider>
+  );
+};
 
 export default App;
