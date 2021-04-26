f289ea59f4c8e5e23c6d585291c010a65c473dc5
Austin <austin@flimflamfactory.com>
2021-04-25 21:43:27 -0500

    docs(studio): remove experimental actions note in site config schema

--- studio/schema/documents/siteConfig.ts
+++ studio/schema/documents/siteConfig.ts
@@ -1,8 +1,6 @@
 import bcp47 from 'bcp47';
 
 export default {
-  // https://www.sanity.io/docs/experimental/ui-affordances-for-actions
-  // __experimental_actions: [/* create, delete, */ 'update', 'publish'],
   __experimental_actions: ['update', 'publish'],
   fields: [
     {
